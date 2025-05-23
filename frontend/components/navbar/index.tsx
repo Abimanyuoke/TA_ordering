3311a23f8d86a1ebb4553c9c940690b4975b6180
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-05-23 19:58:23 +0700

    feat: Refactor layout and components for Sign In and Sign Up pages, update Navbar and Footer, and enhance main Landing Page design

--- frontend/components/navbar/index.tsx
+++ frontend/components/navbar/index.tsx
@@ -1,69 +1,36 @@
 "use client"
 import React from "react";
-import { useState } from "react";
 import Image from "next/image";
-import { IoMdClose } from "react-icons/io";
-import { GiHamburgerMenu } from "react-icons/gi";
-import Link from "next/link";
+import imgLogo from "@/public/image/img - logo.png"
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const handleOpen = () => {
-        setIsOpen(!isOpen);
-    }
+    const router = useRouter();
 
     return (
-        <nav className="bg-white shadow-md sticky w-full top-0 z-50 font-inter px-4">
-            <div className="max-w-7xl mx-auto">
-                <div className="flex p-5 justify-between">
-                    <Image
-                        src="./Logo.svg"
-                        alt="Logo Stuntor"
-                        width={150}
-                        height={150}/>
-                    <div className="hidden md:flex">
-                        <div className="hidden md:flex items-center space-x-8 font-bold text-lg text-[#333333] ">
-                            <a href="#about" className="hover:text-primary transition-all duration-300">Tentang Kami</a>
-                            <a href="#fitur" className="hover:text-primary transition-all duration-300">Fitur</a>
-                            <div className="md:border-2 h-10 mr-4">{''}</div>
-                        </div>
-                        <div className="hidden md:flex space-x-5 font-bold">
-                            <Link
-                                href="/signup"
-                                className="text-primary bg-white px-4 py-2 rounded border-2 border-primary transition-all duration-300">
-                                Daftar
-                            </Link>
-                            <Link
-                                href="/login"
-                                className="text-white bg-primary px-4 py-2 rounded border border-primary transition-all duration-300">
-                                Masuk
-                            </Link>
-                        </div>
-                    </div>
-                    <button onClick={handleOpen} className="text-3xl text-gray-700 md:hidden">
-                        {isOpen ? <IoMdClose /> : <GiHamburgerMenu />}
+        <header className="flex justify-between items-center p-4 pl-13 bg-[#FAFAD2]">
+            <div className="flex items-center gap-2">
+                <Image
+                    src={imgLogo} alt="logo of Plantify" width={40} height={40} className="max-w-full" />
+                <div className="text-3xl font-bold items-center text-[#2E8B57]">Plantify</div>
+            </div>
+
+            <div className="flex items-center gap-8 ml-auto">
+                <nav className="hidden md:flex items-center gap-4 text-[#333333] font-bold">
+                    <a href="#about" className="hover:underline">About Us</a>
+                    <a href="#features" className="hover:underline">Features</a>
+                    <span>|</span>
+                </nav>
+
+                <div className="flex gap-4">
+                    <button className="w-32 cursor-pointer border font-bold bg-white border-[#2E8B57] text-[#2E8B57] px-4 py-2 rounded-lg hover:bg-green-50 transition" onClick={() => router.push("/SignUp")}>
+                        Sign Up
+                    </button>
+                    <button className="w-32 bg-[#2E8B57] cursor-pointer font-bold text-white px-4 py-2 rounded-lg hover:bg-[#276f48] transition" onClick={() => router.push("/SignIn")}>
+                        Sign In
                     </button>
                 </div>
-                {isOpen && (
-                    <div className="absolute top-16 left-0 w-full bg-white shadow-md md:hidden">
-                        <div className="flex flex-col space-y-5 p-4 py-10 font-bold">
-                            <a href="/about" className="text-[#333333] hover:text-primary text-lg">Tentang Kami</a>
-                            <a href="/fitur" className="text-[#333333] hover:text-primary text-lg">Fitur</a>
-                            <Link
-                                href="/signup"
-                                className="text-primary bg-white px-4 py-2 rounded hover:bg-primary hover:text-white border-2 border-primary transition-all duration-300">
-                                Daftar
-                            </Link>
-                            <Link
-                                href="/login"
-                                className="text-white bg-primary px-4 py-2 rounded border border-primary hover:bg-white hover:text-primary transition-all duration-300">
-                                Masuk
-                            </Link>
-                        </div>
-                    </div>
-                )}
             </div>
-        </nav>
+        </header>
     )
 }
